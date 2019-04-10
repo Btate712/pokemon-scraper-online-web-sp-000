0041ddea41c149e4f874165faad4195929cab92f
Robert Tate <btate712@gmail.com>
2019-04-10 22:59:22 +0000

    Automatically backed up by Learn

--- bin/sql_runner.rb
+++ bin/sql_runner.rb
@@ -6,8 +6,6 @@ class SQLRunner
   def execute_schema_migration_sql
     sql = File.read('db/schema_migration.sql')
     execute_sql(sql)
-    sql = File.read('db/alter_table_migration.sql')
-    execute_sql(sql)
   end
 
   def execute_create_hp_column
