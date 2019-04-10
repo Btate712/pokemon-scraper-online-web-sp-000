703a83bfc38f62d82a43213769999591b5ecb2ac
Robert Tate <btate712@gmail.com>
2019-04-10 23:00:57 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,7 +13,7 @@ class Pokemon
 
   def self.find(id, db)
     values = db.execute("SELECT id, name, type, FROM pokemon WHERE id = #{id}")
-    arguments = { :id => values[0][0], :name => values[0][1], :type => values[0][2], }
+    arguments = { :id => values[0][0], :name => values[0][1], :type => values[0][2] }
     self.new(arguments)
   end
 
