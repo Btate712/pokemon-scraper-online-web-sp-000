09c441fbcdb3fc952b4c4a7c54c3673e281cbf9e
Robert Tate <btate712@gmail.com>
2019-04-10 21:41:17 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,14 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  @@all = []
+
+  def initialize(arguments)
+    arguments.each { |key, value| self.send(("#{key}="), value) }
+    @@all << self
+  end
+
+  def self.save(name, type, db)
+    self.new({:name => name, :type => type, :db => db})
+  end
+
 end
