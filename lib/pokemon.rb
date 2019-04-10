46b9a163385dca4b957956d8905429f0cedd6cf0
Robert Tate <btate712@gmail.com>
2019-04-10 21:54:21 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,8 +11,8 @@ class Pokemon
     self.new({:name => name, :type => type, :db => db})
     runner = SQLRunner.new(db)
     runner.execute_sql("INSERT INTO db (name, type) VALUES (?, ?)")
-    runner.execute_sql (name)
-    runner.execute_sql (type)
+    runner.execute_sql(name)
+    runner.execute_sql(type)
   end
 
 end
